0736ce11b137570af1ef04be6435c30d6c10d3c4
maskho <maskhobar@gmail.com>
2023-03-03 07:07:05 +0700

    create database

--- API/Entities/Customer.cs
+++ API/Entities/Customer.cs
@@ -1,12 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace API.Entities
 {
     public class Customer
     {
-        
+        public int Id { get; set; }
+        public string UserName { get; set; }
     }
 }
