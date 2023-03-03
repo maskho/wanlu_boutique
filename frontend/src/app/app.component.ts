857fbfc75c92c103e11e0a6b82d3ebce637b5cb5
maskho <maskhobar@gmail.com>
2023-03-03 16:50:27 +0700

    create http requests

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Wanlu Boutique Analytics';
+export class AppComponent implements OnInit {
+  title = 'Wanlu Boutique';
+  customers: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5005/api/customers').subscribe({
+      next: res => this.customers = res,
+      error: error => console.log(error),
+      complete: () => console.log('request completed')
+    })
+  }
+
 }
